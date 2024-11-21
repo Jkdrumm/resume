5f003a309908127bde5046e584eb6649542e2510
jkdrumm <jkdrumm1@gmail.com>
2024-11-20 19:19:37 -0600

    Added TypeScript color logo

--- src/app/home/resume/skills/TypeScript.tsx
+++ src/app/home/resume/skills/TypeScript.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { ChakraColorIcon } from "@/components/icons/ChakraColor";
 import { SkillUsedDetail } from "../components/SkillUsedDetail";
 import { SiTypescript } from "react-icons/si";
 import { SkillListItemProps } from "../components/SkillList";
+import { TypeScriptColorIcon } from "@/components/icons/TypeScriptColor";
 
 export const TypeScript: React.FC<SkillListItemProps> = (props) => {
   return (
@@ -10,7 +10,7 @@ export const TypeScript: React.FC<SkillListItemProps> = (props) => {
       name="TypeScript"
       description="TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale."
       icon={<SiTypescript />}
-      colorIcon={ChakraColorIcon}
+      colorIcon={TypeScriptColorIcon}
       href="https://www.typescriptlang.org/"
       {...props}
     />
